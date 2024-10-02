6479abd7108a1b37a3036af82943c0bfc7795748
Никита Пронин <pron9nex@MacBook-Air-Nikita.local>
2024-10-02 19:41:18 +0300

    First Commit

---
+++ 1-converter/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const USD = 94
+	const EUR = 122
+	var RUB float64
+	RUB = 150 // For example
+	convertRubToEur := RUB / EUR
+	convertRubToUsd := RUB / USD
+	fmt.Printf("%.2f Rub convert to Euro: %.2f\n", RUB, convertRubToEur)
+	fmt.Printf("%.2f Rub convert to Usd: %.2f\n", RUB, convertRubToUsd)
+
+}
